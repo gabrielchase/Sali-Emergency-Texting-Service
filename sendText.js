2a4f66bcf7b14367bf1450066998e3ee1f48ee2c
gabrielchase <gchase.patron@gmail.com>
2018-05-24 21:10:28 +0800

    Add google maps link and fix text body message

--- sendText.js
+++ sendText.js
@@ -2,20 +2,27 @@ const twilio = require('./twilio')
 
 module.exports = function(req, res) {
     const numbers = req.body.numbers
+    const place = req.body.place
+    const lat = req.body.lat
+    const lng = req.body.lng 
+    
     numbers_array = numbers.split(',')
-    console.log(numbers_array)
     message = 'Sent text to: '
+    console.log('Numbers to send to: ', numbers_array)
+    
     for (let number of numbers_array) {
         message += `${number} `
-        sendText(number)
+        sendText(number, place, lat, lng)
     }
+
     res.status(200).send({ message: message })
 }
 
-function sendText(number) {
+function sendText(number, place, lat, lng) {
     console.log(`sending text to: ${number}`)
+    const google_maps_link = `https://google.com/maps/?q=${lat},${lng}`
     const res = twilio.messages.create({
-        body: '<Name> needs help at <Location>',
+        body: `Emergency at ${place}!\n${google_maps_link}`,
         to: `+63${number}`,
         from:  '+19166341740'
     })
